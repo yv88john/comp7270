db9757b393848b23dfb4d4e8cefd5d6c35bc00d1
yv88john <yv88john@gmail.com>
2020-10-17 17:43:43 +0000

    create update insert qpon

---
+++ comp7270/api/controllers/QponController.js
@@ -0,0 +1,48 @@
+/**
+ * QponController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  create: async function (req, res) {
+
+    if (req.method == "GET") return res.view('qpon/create');
+
+    var qpon = await Qpon.create(req.body).fetch();
+
+    return res.status(201).json({ id: qpon.id });
+  },
+
+
+  update: async function (req, res) {
+
+    if (req.method == "GET") {
+
+        var thatQpon = await Qpon.findOne(req.params.id);
+
+        if (!thatQpon) return res.notFound();
+
+        return res.view('qpon/update', { qpon: thatQpon });
+
+    } else {
+
+        var updatedQpon = await Qpon.updateOne(req.params.id).set(req.body);
+
+        if (!updatedQpon) return res.notFound();
+
+        return res.ok();
+    }
+},
+
+  delete: async function (req, res) {
+
+      var deletedQpon = await Qpon.destroyOne(req.params.id);
+
+      if (!deletedQpon) return res.notFound();
+
+      return res.ok();
+  },
+};
+
---
+++ comp7270/api/models/Qpon.js
@@ -0,0 +1,49 @@
+/**
+ * Qpon.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: {
+      type: "string"
+    },
+
+    restaurant: {
+      type: "string"
+    },
+
+    region: {
+      type:"string"
+    },
+
+    mall: {
+      type:"string"
+    },
+
+    image:{
+      type:"string"
+    },
+
+    quota:{
+      type:"number"
+    },
+
+    coins:{
+      type:"number"
+    },
+
+    expiryDate: {
+      type:"string"
+    },
+
+    detail: {
+      type:"string"
+    }
+  },
+
+};
+
